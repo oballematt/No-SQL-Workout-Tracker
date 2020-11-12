51726f794b409a16eed996f423acf3055652eab4
Matthew Oballe <oballematt@gmail.com>
2020-11-12 12:35:25 -0600

    creating boiler plate code to start server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static('public'));
