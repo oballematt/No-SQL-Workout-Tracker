9863c3d97b84699d5d6f935f3374fa755f1365ec
Matthew Oballe <oballematt@gmail.com>
2020-11-12 13:59:28 -0600

    created api route to create new workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,22 @@
 let db = require("../models");
 
-module.exports = function(app){
-    app.get("/api/workouts", function (req, res){
-        db.Workout.find({}).then(dbWorkout =>{
+module.exports = function (app) {
+    app.get("/api/workouts", function (req, res) {
+        db.Workout.find({}).then(dbWorkout => {
             res.json(dbWorkout)
         })
         .catch(err => {
             res.json(err)
         });
     });
-};
+
+    app.post("/api/workouts", function (req, res) {
+        db.Workout.create({}).then(dbWorkout =>{
+            res.json(dbWorkout)
+        })
+        .catch(err =>{
+            res.json(err)
+        })
+    })
+
+}
