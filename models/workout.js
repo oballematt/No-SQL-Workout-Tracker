3c9a30e6d535bd281a533fd5d07715a13f746640
Matthew Oballe <oballematt@gmail.com>
2020-11-12 12:19:07 -0600

    creaded model folder with workout schema. Required mongoose and mongoose schema in global variable and created workout schema based on seed.js content

---
+++ models/workout.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true
+        },
+        name: {
+          type: String,
+          trim: true
+        },
+        duration: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        }
+      }
+    ]
+  });
