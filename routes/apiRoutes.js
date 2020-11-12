9d6d576aceadc5dd35d39e5cb08dc2e93d79e273
Matthew Oballe <oballematt@gmail.com>
2020-11-12 14:08:05 -0600

    created api route to get workouts/range

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -33,4 +33,13 @@ module.exports = function (app) {
         })
     })
 
+    app.get("api/workouts/range", function (req, res){
+        db.Workout.find({}).then(dbWorkout =>{
+            res.json(dbWorkout)
+        })
+        .catch(err =>{
+            res.json(err)
+        })
+    })
+
 }
